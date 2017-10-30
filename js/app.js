e64e8c899a4c109802ce592948a3513428bed7eb
Douglas Muraoka <douglas.muraoka@gmail.com>
2017-10-30 19:52:09 -0200

    feat: Add player to the game

--- js/app.js
+++ js/app.js
@@ -30,21 +30,54 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x - 2, this.y - 20);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
+    this.reset();
+};
 
+Player.prototype.update = function() {
+    if (this.y < Constants.sizeY) {
+        alert("WIN");
+        this.reset();
+    }
+};
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y - 10);
+};
 
+Player.prototype.handleInput = function(keyPressed) {
+    if (keyPressed === 'left') {
+        if (this.x > 0) {
+            this.x -= Constants.sizeX;
+        }
+    } else if (keyPressed === 'up') {
+        if (this.y > 0) {
+            this.y -= Constants.sizeY;
+        }
+    } else if (keyPressed === 'right') {
+        if (this.x < Constants.maxPosX * Constants.sizeX) {
+            this.x += Constants.sizeX;
+        }
+    } else if (keyPressed === 'down') {
+        if (this.y < Constants.maxPosY * Constants.sizeY) {
+            this.y += Constants.sizeY;
+        }
+    }
+};
+
+Player.prototype.reset = function() {
+    this.x = 2 * Constants.sizeX;
+    this.y = 5 * Constants.sizeY;
+};
 
 var allEnemies = [
     new Enemy(2 * Constants.sizeX, 1 * Constants.sizeY, 300),
     new Enemy(2 * Constants.sizeX, 2 * Constants.sizeY, 100),
     new Enemy(2 * Constants.sizeX, 3 * Constants.sizeY, 200),
 ];
+
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
